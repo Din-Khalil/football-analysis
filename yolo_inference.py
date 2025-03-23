d1f7dff564d085870ad085239f388abed2023735
Din-Khalil <dinkhalil.pro@gmail.com>
2025-03-23 18:10:41 +0100

    Fine tuning

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,8 +1,9 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8x')
+# model = YOLO('yolov8x')
+model = YOLO('models/best.pt')
 
-results = model.predict('input_videos/08fd33_4.mp4', save=True)
+results = model.predict('input_videos/08fd33_4.mp4', save=True, project='.', name='detections' )
 print(results[0])
 print('=========================================')
 for box in results[0].boxes:
